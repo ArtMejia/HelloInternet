22129428bb9b7eea273b9bb58d006f78687f5048
ArtMejia <arteniomejia.student@careerdevs.com>
2022-08-01 19:56:11 -0400

    Initial commit

---
+++ src/test/java/com/mejia/HelloInternet/HelloInternetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mejia.HelloInternet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloInternetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
