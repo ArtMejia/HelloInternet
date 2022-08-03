83c1bebdf4ead7653260833667796b6d2078a57f
ArtMejia <arteniomejia.student@careerdevs.com>
2022-08-02 21:00:53 -0400

    added @PathVariable and @RequestParam

--- src/main/java/com/mejia/helloInternet/controllers/RestController.java
+++ src/main/java/com/mejia/helloInternet/controllers/RestController.java
@@ -5,20 +5,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+// add the @RestController annotation to the newly created class
 @org.springframework.web.bind.annotation.RestController
-
 public class RestController {
 
+    // add the @GetMapping annotation with the path “/hello” given as its parameter
     @GetMapping("/")
+    // Create a method called helloCareerDevs(), that returns a String, and takes no parameters.
     private String helloCareerDevs() {
-       return "You requested the root route";
+        // Implement code so the method returns the string “You requested the root route”
+        return "You requested the root route";
     }
 
+    // Create another get route of your own choosing
     @GetMapping("/login")
     private String login() {
         return "This is where you login";
     }
 
+    // a GET request to the /random path will respond with a random number 1 through 100
     @GetMapping("/random")
     private int randomNumber() {
         Random rand = new Random();
@@ -27,6 +32,7 @@ public class RestController {
         return n;
     }
 
+    // a GET request to the /joke path will respond with a joke. From there try making it pick a random joke from a list of stored jokes (Array of Strings?)
     @GetMapping("/joke")
     private String randomJoke() {
         String[] jokes = {"How do you follow Will Smith in the snow? You follow the fresh prints.",
@@ -39,6 +45,7 @@ public class RestController {
         return jokes[pickAJoke];
     }
 
+    // Try creating a route handler that can utilize a PathVariable
     @GetMapping("/joke/{id}")
     private String getJokeById(@PathVariable int id) {
         String[] jokes = {"How do you follow Will Smith in the snow? You follow the fresh prints.",
@@ -49,6 +56,7 @@ public class RestController {
         return jokes[id];
     }
 
+    // Try creating a route handler that can utilize a RequestParam
     @GetMapping("/jokes/")
     private String getJokeByRequestParam(@RequestParam int id) {
         String[] jokes = {"How do you follow Will Smith in the snow? You follow the fresh prints.",
