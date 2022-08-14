c3316353e4a5b5a6eb82d6061e55fcafad99d07f
ArtMejia <arteniomejia.student@careerdevs.com>
2022-08-14 18:15:19 -0400

    NasaController initial commit

--- src/main/java/com/mejia/helloInternet/controllers/NasaController.java
+++ src/main/java/com/mejia/helloInternet/controllers/NasaController.java
@@ -1,15 +1,21 @@
 package com.mejia.helloInternet.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import com.mejia.helloInternet.models.NasaModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 // // First step is to add the RestController and RequestMapping annotations to the NasaController class
 @RestController
 @RequestMapping("/nasa")
 public class NasaController {
+
+    @Autowired
+    private Environment env;
     // Second step is to create a nasaApodEndpoint field within your new class.
     private final String myNasaKey = "fJmhvO9ivjLyd74qGI9eRnlKtbJ2P3mAPoMNElaG";
     private final String nasaApodEndpoint = "https://api.nasa.gov/planetary/apod?api_key=" + myNasaKey;
@@ -17,8 +23,47 @@ public class NasaController {
 
     // Third step is to add a route handler to your code.
     @GetMapping("/apod")
-    public Object apodHandler (RestTemplate restTemplate) {
-        return restTemplate.getForObject(nasaApodEndpoint, Object.class);
+    public ResponseEntity<?> apodHandler (RestTemplate restTemplate) {
+        try {
+            NasaModel response = restTemplate.getForObject(nasaApodEndpoint, NasaModel.class);
+
+            return ResponseEntity.ok(response);
+
+        } catch (Exception e) {
+            System.out.println(e.getClass());
+            System.out.println(e.getMessage());
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+
+    }
+
+    @GetMapping("/{date}")
+    public ResponseEntity<?> getUserById (RestTemplate restTemplate, @PathVariable String date) {
+        try {
+
+            // throws NumberFormatException if id is not an int
+            Integer.parseInt(date);
+
+            System.out.println("Getting user with date " + date);
+
+            String url = nasaApodEndpoint + "/" + date;
+
+            NasaModel response = restTemplate.getForObject(url, NasaModel.class);
+
+            return ResponseEntity.ok(response);
+
+        } catch (NumberFormatException e) {
+            return ResponseEntity.status(400).body("Date " + date + ", is not a valid date. Must be a date");
+
+        } catch (HttpClientErrorException.NotFound e) {
+            return ResponseEntity.status(404).body("Photo Not Found With Date: " + date);
+
+        } catch (Exception e) {
+            System.out.println(e.getClass());
+            System.out.println(e.getMessage());
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+
     }
 
     // Fourth step is to make a request route that allows you to change the date of the APOD information being requested. You’ll need to use either @PathVariable or @RequestParam, either will work.
---
+++ src/main/java/com/mejia/helloInternet/models/NasaModel.java
@@ -0,0 +1,35 @@
+package com.mejia.helloInternet.models;
+
+public class NasaModel {
+
+    private String date;
+    private String explanation;
+    private String media_type;
+    private String service_version;
+    private String title;
+    private String url;
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getExplanation() {
+        return explanation;
+    }
+
+    public String getMedia_type() {
+        return media_type;
+    }
+
+    public String getService_version() {
+        return service_version;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
